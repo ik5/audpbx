53c4c9e7e03662b7e18464e26cbfbc589899181b
Ido Kanner <idok@linesip.com>
2026-01-10 11:58:16 +0200

    small refactoring, trying to slowly move from magic numbers to constants

--- utils/float_to_int.go
+++ utils/float_to_int.go
@@ -1,13 +1,17 @@
 package utils
 
 func Float32ToInt16(x float32) int16 {
+	const maxInt16 float32 = 32768.0 // 2^15 -> +32767
+
 	// Clamp and scale
 	if x > 1 {
 		x = 1
-	} else if x < -1 {
+	}
+
+	if x < -1 {
 		x = -1
 	}
 
 	// Use 32767 for positive max to avoid overflow
-	return int16(x * 32767.0)
+	return int16(x * maxInt16)
 }
